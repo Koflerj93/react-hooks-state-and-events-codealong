2198c04bb32dfd9d178a6b6dc83ee330be97e14c
github username <github email address>
2021-02-02 06:41:13 -0500

    code along done with notes

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react"; // importing state, using the useState hook
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false); // setting initial state 
+
+  function handleClick() {
+      setIsOn((isOn) => !isOn)
+    // updateing state directly is a no-no!!
+    // isOn = !isOn 
+  }
+  const color = isOn ? "red" : "white";
+
+  return <button style={{ background: color }} onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>; // conditional render if isOn is true display ON else display OFF
+  // onClick method with a handleClick
 }
 
 export default Toggle;
